395e2784c5097c8ebe538b860fd93a2c90c92c43
dungdev <dung.truong@kompa.ai>
2022-03-02 15:26:33 +0700

    update code

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -15,7 +15,6 @@ import Homepage from '../pages/homepage';
 import StockTradingSession from '../pages/stockTradingSession';
 import HistoryStockTrading from '../pages/stockTradingSession/HistoryStockTrading';
 import PersonalProperty from '../pages/personalProperty';
-import CoinTradingPorfolio from '../pages/coin';
 
 const { Content, Footer, Sider } = Layout;
 
@@ -26,10 +25,6 @@ const listMenu = [
   },
   {
     to: '/stock-trading-session',
-    pathName: 'stock-trading-session'
-  },
-  {
-    to: '/coin',
     pathName: 'stock-trading-session'
   }
 ];
@@ -62,9 +57,6 @@ export default function LayoutPage() {
             <Menu.Item key="3" icon={<MoneyCollectOutlined />}>
               <Link to='/personal-property'>Personal Property</Link>
             </Menu.Item>
-            <Menu.Item key="4" icon={<MoneyCollectOutlined />}>
-              <Link to='/coin'>Coin</Link>
-            </Menu.Item>
           </Menu>
         </Sider>
         <Layout className="site-layout">
@@ -77,7 +69,6 @@ export default function LayoutPage() {
               <Switch>
                 <Route exact path='/' component={Homepage} />
                 <Route exact path='/news' component={NewsPage} />
-                <Route exact path='/coin' component={CoinTradingPorfolio} />
                 <Route exact path='/stock-trading-session' component={StockTradingSession} />
                 <Route exact path='/personal-property' component={PersonalProperty} />
                 <Route exact path='/stock-trading-session/history/:tradingKey' component={HistoryStockTrading} />
